7b088745bd7528ba3887e944e9e7dd33d7b86a20
TheMariamE <mariamedwards.smart@gmail.com>
2022-01-17 18:59:43 -0500

    Adjustment to first column of Plus Ultra section

--- src/components/Home/PlusUltra.js
+++ src/components/Home/PlusUltra.js
@@ -2,8 +2,21 @@ import React from 'react';
 
 const PlusUltra = () => {
     return (
-        <div>
-            
+        <div id="plus-ultra" className='container'>
+            <div className='row'>
+                <div className='col-lg-6'>
+                    <div className='col-sm-12 col-md-6 col-lg-5 col-xl-5'>
+                        <div class="alert alert-light" role="alert">Plus Ultra</div>
+                    </div>
+                    <h1>BOKU NO HERO ACADEMIA</h1>
+                    <p>People are not born equal, a realization that four-year-old Izuku Midoriya faced when bullied by his classmates who had unique special powers. Izuku was one of the rare cases where he was born with absolutely no unique powers. This did not stop Izuku from pursuing his dream, a dream of becoming a great hero like the legendary All Might. To become the great hero he hopelessly wants to become, he will now join the ranks of one of the highest rated "Hero Academies" in the country: U.A. High School. </p>
+                    <p>With the help of his idol All Might, will he be able to claim the ranks and become a true hero?</p>
+                    <a class="btn-pu" href="https://boku-no-hero-academia.com/" role="button" rel="noreferrer" target="_blank">Read the manga now!</a>
+                </div>
+                <div className='col-lg-6'>
+
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Principal/TopBar.js
+++ src/components/Principal/TopBar.js
@@ -7,10 +7,10 @@ const TopBar = () => {
             <div className="container tb">
             <div className='row'>
                 <div className="col-lg-7">
-                <p>WATCH MY HERO ACADEMIA´S SEASON 5 NOW ON <a href="https://www.crunchyroll.com/my-hero-academia" target="_blank" rel="noreferrer">CRUNCHYROLL</a>!</p>
+                    <p>WATCH MY HERO ACADEMIA´S SEASON 5 NOW ON <a href="https://www.crunchyroll.com/my-hero-academia" target="_blank" rel="noreferrer">CRUNCHYROLL</a>!</p>
                 </div>
                 <div className="col-lg-5">
-                <p className='manga'>READ MY HERO ACADEMIA´S MANGA <a href="https://boku-no-hero-academia.com/" target="_blank" rel="noreferrer">HERE</a></p>
+                    <p className='manga'>READ MY HERO ACADEMIA´S MANGA <a href="https://boku-no-hero-academia.com/" target="_blank" rel="noreferrer">HERE</a></p>
                 </div>
             </div>
             </div>
