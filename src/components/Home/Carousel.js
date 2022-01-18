1f0e776f09cb59fd0809b7947d8dac8ac7cdc9d7
TheMariamE <mariamedwards.smart@gmail.com>
2022-01-17 19:34:07 -0500

    Added Links to carousel items

--- src/components/Home/Carousel.js
+++ src/components/Home/Carousel.js
@@ -13,60 +13,60 @@ const Carousel = () => {
             <Swiper spaceBetween={20} pagination={{ "dynamicBullets": true }} slidesPerView={3} navigation={true} loop={true} className="mySwiper container">
                 <SwiperSlide>
                     <div class="card hc-season text-white">
-                        <img className="card-img" src={logo} alt="My Hero Academia - Logo"></img>
+                    <a href="https://www3.animeflv.net/anime/boku-no-hero-academia-2016" rel="noreferrer" target="_blank"><img className="card-img" src={logo} alt="My Hero Academia - Logo"></img></a>
                         <div class="card-img-overlay card-body">
                             <h5 className="card-title">
-                                <a href="#" className="stretched-link" rel="noreferrer">Season 1</a>
+                                <a href="https://www3.animeflv.net/anime/boku-no-hero-academia-2016" className="stretched-link" rel="noreferrer" target="_blank">Season 1</a>
                             </h5>
                         </div>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
                     <div class="card hc-season text-white">
-                        <img className="card-img" src={logo} alt="My Hero Academia - Logo"></img>
+                    <a href="https://www3.animeflv.net/anime/boku-no-hero-academia-2nd-season" rel="noreferrer" target="_blank"><img className="card-img" src={logo} alt="My Hero Academia - Logo"></img></a>
                         <div class="card-img-overlay card-body">
                             <h5 className="card-title">
-                                <a href="#" className="stretched-link" rel="noreferrer">Season 2</a>
+                                <a href="https://www3.animeflv.net/anime/boku-no-hero-academia-2nd-season" className="stretched-link" rel="noreferrer" target="_blank">Season 2</a>
                             </h5>
                         </div>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
                     <div class="card hc-season text-white">
-                        <img className="card-img" src={logo} alt="My Hero Academia - Logo"></img>
+                    <a href="https://www3.animeflv.net/anime/boku-no-hero-academia-3rd-season" rel="noreferrer" target="_blank"><img className="card-img" src={logo} alt="My Hero Academia - Logo"></img></a>
                         <div class="card-img-overlay card-body">
                             <h5 className="card-title">
-                                <a href="#" className="stretched-link" rel="noreferrer">Season 3</a>
+                                <a href="https://www3.animeflv.net/anime/boku-no-hero-academia-3rd-season" className="stretched-link" rel="noreferrer" target="_blank">Season 3</a>
                             </h5>
                         </div>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
                     <div class="card hc-season text-white">
-                        <img className="card-img" src={logo} alt="My Hero Academia - Logo"></img>
+                    <a href="https://www3.animeflv.net/anime/boku-no-hero-academia-4th-season" rel="noreferrer" target="_blank"><img className="card-img" src={logo} alt="My Hero Academia - Logo"></img></a>
                         <div class="card-img-overlay card-body">
                             <h5 className="card-title">
-                                <a href="#" className="stretched-link" rel="noreferrer">Season 4</a>
+                                <a href="https://www3.animeflv.net/anime/boku-no-hero-academia-4th-season" className="stretched-link" rel="noreferrer" target="_blank">Season 4</a>
                             </h5>
                         </div>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
                     <div class="card hc-season text-white">
-                        <img className="card-img" src={logo} alt="My Hero Academia - Logo"></img>
+                    <a href="https://www3.animeflv.net/anime/boku-no-hero-academia-5th-season" rel="noreferrer" target="_blank"><img className="card-img" src={logo} alt="My Hero Academia - Logo"></img></a>
                         <div class="card-img-overlay card-body">
                             <h5 className="card-title">
-                                <a href="#" className="stretched-link" rel="noreferrer">Season 5</a>
+                                <a href="https://www3.animeflv.net/anime/boku-no-hero-academia-5th-season" className="stretched-link" rel="noreferrer" target="_blank">Season 5</a>
                             </h5>
                         </div>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
                     <div class="card hc-season text-white">
-                        <img className="card-img" src={logo} alt="My Hero Academia - Logo"></img>
+                    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" rel="noreferrer" target="_blank"><img className="card-img" src={logo} alt="My Hero Academia - Logo"></img></a>
                         <div class="card-img-overlay card-body">
                             <h5 className="card-title">
-                                <a href="#" className="stretched-link" rel="noreferrer">Season 6</a>
+                                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="stretched-link" rel="noreferrer" target="_blank">Season 6</a>
                             </h5>
                         </div>
                     </div>
