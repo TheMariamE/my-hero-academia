5b4214852aa90641237a4d6a4cc6dcc8acdd4a68
TheMariamE <mariamedwards.smart@gmail.com>
2022-02-20 18:49:01 -0500

    Added the follow us logo section

--- src/components/Principal/Footer.js
+++ src/components/Principal/Footer.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import logo from "../../images/logo.png";
+import fandom from "../../images/fandom.png";
+import insta from "../../images/instagram.png";
+import wiki from "../../images/wikipedia.png";
+import twitter from "../../images/twitter.png";
+import imdb from "../../images/imdb.png";
+import tt from "../../images/tiktok.png";
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
@@ -58,6 +64,46 @@ const Footer = () => {
                         </div>
                         <div className='col-lg-2'>
                             <h6>Follow Us</h6>
+                            <div className="row f-logos">
+                                <div className="col-lg-6">
+                                    <ul className="footer-nav">
+                                        <li>
+                                            <a className="nav-link" href="https://myheroacademia.fandom.com/" target="_blank" rel="noreferrer">
+                                                <img src={fandom} alt="My Hero Academia - Fandom"></img>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a className="nav-link" href="https://www.instagram.com/my.hero.academia/" target="_blank" rel="noreferrer">
+                                                <img src={insta} alt="My Hero Academia - Instagram"></img>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a className="nav-link" href="https://en.wikipedia.org/wiki/My_Hero_Academia" target="_blank" rel="noreferrer">
+                                                <img src={wiki} alt="My Hero Academia - Wikipedia"></img>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="col-lg-6">
+                                    <ul className="footer-nav">
+                                        <li>
+                                            <a className="nav-link" href="https://twitter.com/MHAOfficial" target="_blank" rel="noreferrer">
+                                                <img src={twitter} alt="My Hero Academia - Twitter"></img>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a className="nav-link" href="https://www.imdb.com/title/tt5626028/" target="_blank" rel="noreferrer">
+                                                <img id="imdb-logo" src={imdb} alt="My Hero Academia - IMDB"></img>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a className="nav-link" href="https://www.tiktok.com/@myheroacademiagame?lang=en" target="_blank" rel="noreferrer">
+                                                <img src={tt} alt="My Hero Academia - Tik Tok"></img>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -69,7 +115,7 @@ const Footer = () => {
                             <p>Boku no Hero Academia</p>
                         </div>
                         <div className='col-lg-6'>
-                            <p>Copyright © 2021. No rights reserved. | Web Design by Mariam Edwards.</p>
+                            <p>All credit goes to the content owners. | Web Design by Mariam Edwards.</p>
                         </div>
                     </div>
                 </div>
