296a84aaf4b881edef517240dc9b9f0d33974dd6
TheMariamE <mariamedwards.smart@gmail.com>
2022-03-02 22:23:43 -0500

    Small adjustments to style

--- src/components/Author/SO.js
+++ src/components/Author/SO.js
@@ -12,17 +12,17 @@ const SO = () => {
             <div className='row'>
                 <div className='col-lg-12'>
                     <h2>SERIES & Oneshots</h2>
-                    <Swiper spaceBetween={25} slidesPerView={3} navigation={true} loop={true} className="mySwiper container">
+                    <Swiper spaceBetween={27} slidesPerView={3} navigation={true} loop={true} className="mySwiper container">
                             {seriesoneshots.map((item) => (
                                 <SwiperSlide>
-                                <div className="so-container-overlay">
+                                <a className="so-container-overlay" href={item.url} rel="noreferrer" target="_blank">
                                     <img src={item.img} alt="Avatar" className="so-image"></img>
                                     <div className="so-overlay">
                                         <h2 className="so-text">{item.title}</h2>
                                         <p className="so-p">{item.genre}</p>
                                         <p className="so-p">{item.year}</p>
                                     </div>
-                                </div>
+                                </a>
                                 </SwiperSlide>
                             ))}
                     </Swiper>
--- src/components/Author/seriesoneshots.js
+++ src/components/Author/seriesoneshots.js
@@ -5,37 +5,43 @@ const seriesoneshots = [
       img: images.so1,
       title: 'Sensei no Baruji',
       genre: 'Adventure, fantasy',
-      year: '2012'
+      year: '2012',
+      url: 'https://twitter.com/horikoshiko'
     },
     {
       img: images.so2,
       title: 'Sensei no Baruji',
       genre: 'Adventure, fantasy',
-      year: '2012'
+      year: '2012',
+      url: 'https://twitter.com/horikoshiko'
     },
     {
       img: images.so3,
       title: 'Sensei no Baruji',
       genre: 'Adventure, fantasy',
-      year: '2012'
+      year: '2012',
+      url: 'https://twitter.com/horikoshiko'
     },
     {
       img: images.so3,
       title: 'Sensei no Baruji',
       genre: 'Adventure, fantasy',
-      year: '2012'
+      year: '2012',
+      url: 'https://twitter.com/horikoshiko'
     },
     {
-      img: images.so3,
+      img: images.so2,
       title: 'Sensei no Baruji',
       genre: 'Adventure, fantasy',
-      year: '2012'
+      year: '2012',
+      url: 'https://twitter.com/horikoshiko'
     },
     {
-      img: images.so3,
+      img: images.so2,
       title: 'Sensei no Baruji',
       genre: 'Adventure, fantasy',
-      year: '2012'
+      year: '2012',
+      url: 'https://twitter.com/horikoshiko'
     }
   ];
 
